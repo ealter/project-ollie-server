8e0c9d7e48ca3887d7e4ef981a4cf5904cabd77c
Eliot Alter <Eliot.Alter@tufts.edu>
2012-06-03 09:25:30 +0000

    Made the recovery email send using html

--- www/accounts/sendRecoveryEmail.php
+++ www/accounts/sendRecoveryEmail.php
@@ -13,12 +13,16 @@ function doesEmailExist($email) {
 function sendRecoveryEmail($email, $recoveryLink) {
   //TODO: abstract out the game name into a constant
   $subject = "Recover password for Gorilla Warfare";
-  $body = "This email has been sent to you automatically from Gorilla Warefare
-  in response to a request to recover your password.\n
-  If you did not ask to recover your password, please ignore this email.\n\n
-  To reset your password, please click on this link
-  <a href=\"$recoveryLink\">$recoveryLink</a>";
-  $headers = "From: eliot.alter@tufts.edu\r\n"; //TODO: change this
+  $body = "<html><body>
+    <p>This email has been sent to you automatically from Gorilla Warefare
+    in response to a request to recover your password.</p>
+    <p>If you did not ask to recover your password, please ignore this email.</p>
+    <p>To reset your password, please click on this link
+  <a href=\"$recoveryLink\">$recoveryLink</a></p></body></html>";
+  $from = 'eliot.alter@tufts.edu'; //TODO: change email address
+  $headers = "MIME-Version: 1.0\r\n" .
+             "Content-type: text/html\r\n" .
+             "From: $from\r\n";
   return mail($email, $subject, $body, $headers);
 }
 
@@ -28,5 +32,4 @@ if(!doesEmailExist($email)) {
   exit;
 }
 $link = Accounts::getPasswordResetLink($email);
-echo $link;
 sendRecoveryEmail($email, $link);
