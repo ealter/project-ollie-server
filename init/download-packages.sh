ab7169d15088d731824d7c718bd7c686a4cb0bd7
Eliot Alter <Eliot.Alter@tufts.edu>
2012-06-04 21:56:16 +0000

    Added nodejs stuff to the download packages list

--- init/download-packages.sh
+++ init/download-packages.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-apt-get install vim nginx mongo php5 php5-fpm php5-cgi sendmail
+apt-get install python-software-properties
+apt-add-repository ppa:chris-lea/node.js
+apt-get update
+apt-get install vim nginx mongo php5 php5-fpm php5-cgi sendmail nodejs npm
+npm install mongojs journey
