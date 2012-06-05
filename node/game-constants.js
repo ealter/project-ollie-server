7c3872d445eb368fdfcc0795740558c9440cba5d
Eliot Alter <Eliot.Alter@tufts.edu>
2012-06-05 09:54:11 -1000

    Added a file to abstract out a few game constants

---
+++ node/game-constants.js
@@ -0,0 +1,4 @@
+/* Exports various global constants about the game */
+exports.hostname = 'localhost';
+exports.gamename = 'Gorilla Warefare';
+exports.noReplyEmail = 'M8R-x1hmki@mailinator.com' //gorilla-warefare@mailinator
