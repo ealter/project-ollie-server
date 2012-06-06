79bf011a2e29431fc9e2be4eb683cdd276e4d87d
Eliot Alter <Eliot.Alter@tufts.edu>
2012-06-05 17:35:37 -1000

    Changed the node packages installer to use the forked version of journey, instead of the npm one

--- node/install-node-packages.sh
+++ node/install-node-packages.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-npm install mongodb mongojs journey password-hash nodemailer mustache
+npm install mongodb mongojs password-hash nodemailer mustache
 npm install -g supervisor
+cd node_modules
+git clone git://github.com/ealter/journey.git
