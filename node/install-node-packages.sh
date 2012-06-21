483cb5339f44c533d7bd7f71337cb3a21755112f
Eliot Alter <Eliot.Alter@tufts.edu>
2012-06-21 12:28:25 -1000

    Added package.json to handle dependencies

--- node/install-node-packages.sh
+++ node/install-node-packages.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-npm install mongodb mongojs password-hash nodemailer mustache
-npm install -g supervisor
+sudo npm install
 cd node_modules
 git clone git://github.com/ealter/journey.git
