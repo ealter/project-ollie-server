65595bc9a8d4d6ca10013686b0a84346f40d1778
Eliot Alter <Eliot.Alter@tufts.edu>
2012-07-05 13:11:19 -1000

    Changed the hostname from localhost to the correct url

--- node/game-constants.js
+++ node/game-constants.js
@@ -1,5 +1,5 @@
 /* Exports various global constants about the game */
-exports.hostname = 'localhost';
+exports.hostname = 'www.gorillawarefaregame.com';
 exports.gamename = 'Gorilla Warefare';
 exports.noReplyEmail = 'M8R-x1hmki@mailinator.com' //gorilla-warefare@mailinator
 exports.minPasswordLength = 6;
