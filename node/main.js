6c18f609f2b203fed3868a96a1b7266b5a948432
Eliot Alter <Eliot.Alter@tufts.edu>
2012-07-06 12:39:24 -1000

    Added the option to specify the port

--- node/main.js
+++ node/main.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var router = require('./router').createRouter();
 
-var port = 1337;
+var port = process.argv[2] || 1337;
 http.createServer(function (request, response) {
   var body = '';
   request.on('data', function (chunk) {
