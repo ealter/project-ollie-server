bf99e9501db49ad6a01db81aa34d97d19be7d03d
Eliot Alter <Eliot.Alter@tufts.edu>
2012-08-02 14:45:07 -1000

    Fixed a stupid misspelling

--- node/game-constants.js
+++ node/game-constants.js
@@ -1,7 +1,7 @@
 /* Exports various global constants about the game */
 var mongojs = require('mongojs');
 
-exports.hostname = 'gorillawarefaregame.com';
+exports.hostname = 'gorillawarfaregame.com';
 exports.gamename = 'Gorilla Warfare';
 exports.noReplyEmail = 'no-reply@' + exports.hostname;
 exports.minPasswordLength = 1;
